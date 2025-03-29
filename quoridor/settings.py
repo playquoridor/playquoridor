f686bffccf744d72fa36d6ef31f5844ee3d04972
Ramon Viñas <rvinas@users.noreply.github.com>
2025-03-29 18:37:31 +0100

    Update settings.py

--- quoridor/settings.py
+++ quoridor/settings.py
@@ -128,6 +128,21 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_REDIRECT_URL = "/"  # Redirect users after login
+LOGOUT_REDIRECT_URL = "/"  # Redirect users after logout
+
+# Google authentication
+GOOGLE_OAUTH_CLIENT_ID = os.environ.get('GOOGLE_OAUTH_CLIENT_ID')
+if not GOOGLE_OAUTH_CLIENT_ID:
+    raise ValueError(
+        'GOOGLE_OAUTH_CLIENT_ID is missing.'
+        'Have you put it in .env ?'
+    )
+
+# We need these lines below to allow the Google sign in popup to work.
+SECURE_REFERRER_POLICY = 'no-referrer-when-downgrade'
+SECURE_CROSS_ORIGIN_OPENER_POLICY = "same-origin-allow-popups"
+
 # Rest framework settings
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
